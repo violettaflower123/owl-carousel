8791d47b695d5985ae183cb21b541004ffe50a69
unknown <vitaminka_94@mail.ru>
2022-10-24 12:53:15 +0300

    fix: create slider

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// owl carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: true,
+    center: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      }
+    },
+  });
+
+
+});
